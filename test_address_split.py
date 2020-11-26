25ef140c8dab1dc1b8789014fd28dcb5d8848826
391209 <ajaytnk.sahu@gmail.com>
2020-11-26 13:57:31 +0100

    test case updated

---
+++ test_address_split.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+from address_split import *
+
+
+class TestAddress(TestCase):
+
+    def test_streetname_number(self):
+        address1 = "Midscheeps 1"
+        street_addr, house_nr_str = Address.address_split(address1)
+        assert street_addr == 'Midscheeps', "Should be Midscheeps"
+        assert house_nr_str == '1', "Should be 1"
+
+    def test_null(self):
+        address1 = ""
+        street_addr, house_nr_str = Address.address_split(address1)
+        assert street_addr == '', "Should be empty"
+        assert house_nr_str == "none", "Should be None"
