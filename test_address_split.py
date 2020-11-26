7dd3287dd6a63381bb3d53a08720f92882f313d2
391209 <ajaytnk.sahu@gmail.com>
2020-11-26 14:35:05 +0100

    test updated

--- test_address_split.py
+++ test_address_split.py
@@ -6,12 +6,12 @@ class TestAddress(TestCase):
 
     def test_streetname_number(self):
         address1 = "Midscheeps 1"
-        street_addr, house_nr_str = Address.address_split(address1)
+        street_addr, house_nr_str = address_split(address1)
         assert street_addr == 'Midscheeps', "Should be Midscheeps"
         assert house_nr_str == '1', "Should be 1"
 
     def test_null(self):
         address1 = ""
-        street_addr, house_nr_str = Address.address_split(address1)
+        street_addr, house_nr_str = address_split(address1)
         assert street_addr == '', "Should be empty"
         assert house_nr_str == "none", "Should be None"
